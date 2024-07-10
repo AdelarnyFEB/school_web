f23e8163c64ca184177ea2ef97e99c7db14d598a
Adelani Abidemi <80181236+AdelarnyFEB@users.noreply.github.com>
2024-07-10 20:05:06 +0100

    first commit

---
+++ frontend/src/components/Home.jsx
@@ -0,0 +1,30 @@
+import React from 'react'
+import {
+    Navbar,
+    Logo,
+    NavigationLinks,
+    NavLink,
+    ButtonsContainer,
+    LoginButton,
+    GuestButton,
+    HomeContainer,
+    SchoolInfo,
+    SchoolImage,
+    Title,
+    LoremTextContainer,
+    AdminRegisterLink
+} from '../styles/styles'
+import { LoremIpsum } from 'lorem-ipsum';
+import bg from '../assets/bg.png';
+import bg1 from '../assets/bg1.png'
+import { Link, useNavigate } from 'react-router-dom';
+
+const Home = () => {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default Home
